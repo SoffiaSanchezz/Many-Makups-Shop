ec5e1b44956f238a664213f45d7f51d548a9aa65
Sofia Sanchez <ssofiiadayanna@gmail.com>
2022-12-28 23:54:13 -0500

    Progress

---
+++ src/Components/NavResponsive.jsx
@@ -0,0 +1,25 @@
+
+import { BsCart3 } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+
+import Logo from '../assets/Logo.png'
+
+const NavResponsive = () => {
+    return (
+        <nav className="w-full  fixed top-0 flex justify-between items-center px-40 text-2xl  gap-4 md:px-6 z-40">
+            <div className="flex flex-row">
+                <Link to="/">
+                    <img className="w-40 left-44 md:w-20 justify-center" src={Logo} alt="" />
+                </Link>
+            </div>
+            <p className="font-Lobster">
+                Many Makups
+            </p>
+            <div>
+                <BsCart3 className="flex flex-row items-end md:text-4xl" />
+            </div>
+        </nav>
+    )
+}
+
+export default NavResponsive
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import NavResponsive from '../Components/NavResponsive'
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <NavResponsive />
+
+      <section className='bottom-5'>
+        <div className='center flex-col gap-5 w-full md:flex-row'>
+          <div className="bg-clip-text text-transparent h-full flex flex-col justify-center relative center w-full  md:w-2/4"></div>
+          <h2 className='text-6xl font-Amatic text-center select-none uppercase text-[#F6B2BF] sm:text-8xl xl:text-[120px]'>
+            Bienvenidos
+          </h2>
+          <div className='flex fle justify-center'>
+          <p className='font-Amatic text-5xl letf-2'>A</p>
+          <p className='font-Amatic text-5xl'><br/>Many Makups</p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
