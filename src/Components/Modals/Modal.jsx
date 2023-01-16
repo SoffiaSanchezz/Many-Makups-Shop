e3c10bef979b3026c3aa68579b43a8d5915c0c80
Sofia Sanchez <ssofiiadayanna@gmail.com>
2023-01-16 03:00:38 -0500

    progress Categories

--- src/Components/Modals/Modal.jsx
+++ src/Components/Modals/Modal.jsx
@@ -14,7 +14,6 @@ import Brochh from '../../assets/Producto13.jpeg'
 
 const Modal = () => {
     const [modals, setModal] = useState(false)
-
     const [Espeji, setEspejo] = useState(false)
     const [CosMe, setOpen] = useState(false)
     const [Boti, SetSection] = useState(false)
@@ -24,37 +23,6 @@ const Modal = () => {
     return (
         <>
             <div className="grid grid-cols-2 justify-around md:grid-cols-4 p-2 gap-5">
-
-            {/* img One  */}
-                <div className="" onClick={() => setEspejo(!Espeji)}>
-                    <div className="bg-purple-100 p-5 rounded-2xl">
-                        <img size={40} src={Espejos} alt="Cosmetiquera" className=" rounded-3xl hover:scale-105" />
-                        <h2 className="text-center text-3xl">Espejos Trendy</h2>
-                        <div className='flex flex-row justify-between'>
-                            <p className="text-pink-500 font-Lobster text-2xl">$15.000</p>
-                            <TbShoppingCartPlus  className="text-3xl"/>
-                        </div>
-                    </div>
-                </div>
-                    {Espeji && (
-                        <div className=" fixed z-[90] inline-block h-98 w-98 px-10 py-10 top-40 right-5 left-5 rounded-lg border-4 border-[#f65c24] flex-col  gap-4 font-Handlee text-3xl bg-[#FFDFD3] drop-shadow-2xl text-center">
-                            <button
-                                onClick={() => setEspejo(!Espeji)} className='absolute top-4 right-5'>
-                                <RiCloseFill size={33} className='fill-light cursor-pointer bg-[#0005] border-2 rounded-full border-black  hover:text-purple-900 hover:border-purple-900' />
-                            </button>
-                            <h4 className="text-center font-Amatic text-5xl">
-                                Espejos Paleta Trendy
-                            </h4>
-                            <br />
-                            <p className="text-center text-xl flex md:text-2xl flex-row-reverse">
-                                ¡Nada mejor que tener un espejo de mano para maquillarte a detalle!
-                                Espejo de mano con forma cuadrada.Diseño exclusivos Trendy. Tiene un mango central de peso equilibrado. Es perfecto para tus viajes, retocar o maquillar en cualquier lugar.
-                                <img src={Espejito} className=' w-48'/>
-                            </p>
-
-                        </div>
-                    )}
-
                     {/* img two */}
                     <div className="" onClick={() => setModal(!modals)}>
                     <div className="bg-purple-100 p-5 rounded-2xl">
@@ -86,6 +54,37 @@ const Modal = () => {
 
                         </div>
                     )}
+
+            {/* img One  */}
+                <div className="" onClick={() => setEspejo(!Espeji)}>
+                    <div className="bg-purple-100 p-5 rounded-2xl">
+                        <img size={40} src={Espejos} alt="Cosmetiquera" className=" rounded-3xl hover:scale-105" />
+                        <h2 className="text-center text-3xl">Espejos Trendy</h2>
+                        <div className='flex flex-row justify-between'>
+                            <p className="text-pink-500 font-Lobster text-2xl">$15.000</p>
+                            <TbShoppingCartPlus  className="text-3xl"/>
+                        </div>
+                    </div>
+                </div>
+                    {Espeji && (
+                        <div className=" fixed z-[90] inline-block h-98 w-98 px-10 py-10 top-40 right-5 left-5 rounded-lg border-4 border-[#f65c24] flex-col  gap-4 font-Handlee text-3xl bg-[#FFDFD3] drop-shadow-2xl text-center">
+                            <button
+                                onClick={() => setEspejo(!Espeji)} className='absolute top-4 right-5'>
+                                <RiCloseFill size={33} className='fill-light cursor-pointer bg-[#0005] border-2 rounded-full border-black  hover:text-purple-900 hover:border-purple-900' />
+                            </button>
+                            <h4 className="text-center font-Amatic text-5xl">
+                                Espejos Paleta Trendy
+                            </h4>
+                            <br />
+                            <p className="text-center text-xl flex md:text-2xl flex-row-reverse">
+                                ¡Nada mejor que tener un espejo de mano para maquillarte a detalle!
+                                Espejo de mano con forma cuadrada.Diseño exclusivos Trendy. Tiene un mango central de peso equilibrado. Es perfecto para tus viajes, retocar o maquillar en cualquier lugar.
+                                <img src={Espejito} className=' w-48'/>
+                            </p>
+
+                        </div>
+                    )}
+
 
                 {/* img three */}
                 <div className="" onClick={() => setOpen(!CosMe)}>
@@ -127,6 +126,22 @@ const Modal = () => {
                         </div>
                     </div>
                 </div>
+                {CosMe && (
+                        <div className=" fixed z-[90] inline-block h-98 w-98 px-10 py-10 top-40 right-5 left-5 rounded-lg border-4 border-[#f65c24] flex-col  gap-4 font-Handlee text-3xl bg-[#FFDFD3] drop-shadow-2xl text-center">
+                            <button
+                                onClick={() => setOpen(!CosMe)} className='absolute top-4 right-5'>
+                                <RiCloseFill size={40} className='fill-light cursor-pointer bg-[#0005] border-2 rounded-full border-black  hover:text-purple-900 hover:border-purple-900' />
+                            </button>
+                            <h4 className="text-center font-Amatic text-5xl">
+                                Cosmetiquera 
+                            </h4>
+                            <br />
+                            <p className="text-center">
+                            Mejora la circulación facial, mantiene la piel uniforme y tersa gracias a sus propiedades frías, ayuda a controlar signos de la edad, mantiene la firmeza, estimula las células de colágeno, para que empiecen a actuar por ellas mismas. Son hermosas, y super espaciosas, su material es super resistente y fácil de limpiar lo que quiere decir que no se va a pelar, mantiene tus implementos de maquillaje intactos y sin temor a que se te  pierdan.
+                            </p>
+
+                        </div>
+                    )}
                     <div className="" onClick={() => setOpen(!CosMe)}>
                     <div className="bg-purple-100 p-5 rounded-2xl">
                         <img size={40} src={Cartu} alt="Cosmetiquera" className=" rounded-3xl hover:scale-105" />
@@ -137,6 +152,22 @@ const Modal = () => {
                         </div>
                     </div>
                 </div>
+                {CosMe && (
+                        <div className=" fixed z-[90] inline-block h-98 w-98 px-10 py-10 top-40 right-5 left-5 rounded-lg border-4 border-[#f65c24] flex-col  gap-4 font-Handlee text-3xl bg-[#FFDFD3] drop-shadow-2xl text-center">
+                            <button
+                                onClick={() => setOpen(!CosMe)} className='absolute top-4 right-5'>
+                                <RiCloseFill size={40} className='fill-light cursor-pointer bg-[#0005] border-2 rounded-full border-black  hover:text-purple-900 hover:border-purple-900' />
+                            </button>
+                            <h4 className="text-center font-Amatic text-5xl">
+                                Cosmetiquera 
+                            </h4>
+                            <br />
+                            <p className="text-center">
+                            Mejora la circulación facial, mantiene la piel uniforme y tersa gracias a sus propiedades frías, ayuda a controlar signos de la edad, mantiene la firmeza, estimula las células de colágeno, para que empiecen a actuar por ellas mismas. Son hermosas, y super espaciosas, su material es super resistente y fácil de limpiar lo que quiere decir que no se va a pelar, mantiene tus implementos de maquillaje intactos y sin temor a que se te  pierdan.
+                            </p>
+
+                        </div>
+                    )}
 
                 {/* img five */}
 
@@ -182,6 +213,7 @@ const Modal = () => {
                         </div>
                     </div>
                 </div>
+                
 
                     <div className="" onClick={() => SetBoch(!Brosh)}>
                     <div className="bg-purple-100 p-5 rounded-2xl">
@@ -212,7 +244,7 @@ const Modal = () => {
                         </div>
                     )}
             </div>
-                </>              
+        </>              
     )
 }
 
